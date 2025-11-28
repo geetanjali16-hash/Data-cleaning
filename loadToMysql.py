0e307fa262a5a1fc8e5f73c0ce93cb4b571df677
geetanjali16-hash <geetanjali16@gmail.com>
2025-11-28 20:29:29 +0530

    Create loadToMysql.py

---
+++ loadToMysql.py
@@ -0,0 +1,33 @@
+import pymysql
+
+# To connect MySQL database
+# Enter your own values for the parameters
+conn = pymysql.connect(
+        host='localhost', # Your Mysql Server name
+        user='root', # Your Mysql Username
+        password="", # Your Mysql Password
+        db='database_gfg',# Your Mysql database name
+)
+
+cur = conn.cursor()
+table_name = "products"
+# Create a table and adjust the Schema as needed
+sql = f"""
+       CREATE TABLE IF NOT EXISTS {table_name} (RowID INT PRIMARY KEY AUTO_INCREMENT,
+       OrderID  VARCHAR(50) NOT NULL, OrderDate DATE, ShipDate DATE,
+       ShipMode VARCHAR(50) NOT NULL,
+       CustomerID VARCHAR(50) NOT NULL, CustomerName VARCHAR(50) NOT NULL,
+       Segment VARCHAR(50) NOT NULL, Country VARCHAR(50) NOT NULL, City VARCHAR(50) NOT NULL,
+       State VARCHAR(50) NOT NULL, PostalCode INT,Region VARCHAR(50) NOT NULL,
+       ProductID VARCHAR(50) NOT NULL,Category VARCHAR(50) NOT NULL,
+       SubCategory VARCHAR(50) NOT NULL,
+       ProductName VARCHAR(50) NOT NULL, Sales DECIMAL(5,4)
+       )
+       """
+
+# table created
+cur.execute(sql)
+conn.commit()
+
+# To close the connection
+conn.close()
